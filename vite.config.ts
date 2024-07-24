253a98b269cbda4e5d6259de33b99ade133a59a2
sasha <suveikosasha@gmail.com>
2024-07-24 17:37:39 +0300

    chore: mini app

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+export default defineConfig({
+	plugins: [react(), basicSsl()],
+	build: {
+		outDir: './docs'
+	},
+	base: './'
+});
+
